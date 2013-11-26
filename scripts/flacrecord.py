5a6fe363577ba8b21dbdf231b81a0f2ee7a6b601
qgodefroi <qgodefroi@gmail.com>
2013-11-26 10:42:55 +0000

    Add override and speech topic

--- scripts/flacrecord.py
+++ scripts/flacrecord.py
@@ -10,11 +10,11 @@ def find_input_device(pyaudio):
         device_index = None            
         for i in range( pyaudio.get_device_count() ):     
             devinfo = pyaudio.get_device_info_by_index(i)   
-            #print( "Device %d: %s"%(i,devinfo["name"]) )
+            print( "Device %d: %s"%(i,devinfo["name"]) )
 
             for keyword in ["primesense","input"]:
                 if keyword in devinfo["name"].lower():
-                    #print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
+                    print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
                     device_index = i
                     return device_index
 
@@ -43,61 +43,65 @@ def cancel_interaction():
 
 def listen_for_block_of_speech():
 
-    #config
-    chunk = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
-    PRE_SAMPLES = 10
-    SILENCE_LIMIT = 1 #Silence limit in seconds. The max ammount of seconds where only silence is recorded. When this time passes the recording finishes and the file is delivered.
+	#config
+	chunk = 1024
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 1
+	RATE = 16000
+	PRE_SAMPLES = 10
+	SILENCE_LIMIT = 1 #Silence limit in seconds. The max ammount of seconds where only silence is recorded. When this time passes the recording finishes and the file is delivered.
 
-    #open stream
-    p = pyaudio.PyAudio()
+	#open stream
+	p = pyaudio.PyAudio()
 
-    stream = p.open(format = FORMAT,
+	stream = p.open(format = FORMAT,
                     channels = CHANNELS,
                     rate = RATE,
                     input = True,
                     input_device_index = find_input_device(p),
                     frames_per_buffer = chunk)
 
-    THRESHOLD = calibrate_input_threshold(stream, chunk)
+	THRESHOLD = calibrate_input_threshold(stream, chunk)
 
-    all_m = []
-    data = ''
-    SILENCE_LIMIT = 2
-    rel = RATE/chunk
-    slid_win = deque(maxlen=SILENCE_LIMIT*rel)
-    started = False
-    global finished
-    finished = False
+	all_m = []
+	data = ''
+	SILENCE_LIMIT = 2
+	rel = RATE/chunk
+	slid_win = deque(maxlen=SILENCE_LIMIT*rel)
+	started = False
+	global finished
+	finished = False
 
-    play_wav(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'raw/soundstart.wav'))
+	play_wav(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'raw/soundstart.wav'))
     
-    while (not finished):
-        data = stream.read(chunk)
-        slid_win.append (abs(audioop.avg(data, 2)))
-        if(True in [ x>THRESHOLD for x in slid_win]):
-            if(not started):
-                print("starting to record")
-            started = True
-            all_m.append(data)
-        elif (started==True):
-            wav_filename = save_speech(all_m,p)
-            flac_filename = convert_wav_to_flac(wav_filename)
-            #reset all
-            started = False
-            finished = True
-        else:
+	while (not finished):
+		data = stream.read(chunk)
+		slid_win.append (abs(audioop.avg(data, 2)))
+		if(True in [ x>THRESHOLD for x in slid_win]):
+			if(not started):
+				print("starting to record")
+			started = True
+			all_m.append(data)
+		elif (started==True):
+			wav_filename = save_speech(all_m,p)
+			flac_filename = convert_wav_to_flac(wav_filename)
+			#reset all
+			started = False
+			finished = True
+		else:
 			if len(all_m) > PRE_SAMPLES:
 				all_m.pop(0)
 			all_m.append(data)
 
-    play_wav(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'raw/soundstop.wav'))
+	play_wav(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'raw/soundstop.wav'))
 
-    stream.close()
-    p.terminate()
-    return flac_filename
+	stream.close()
+	p.terminate()
+
+	if 'flac_filename' in locals():
+		return flac_filename
+	else:
+		return []
 
 def save_speech(data, p):
     filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),'output_'+str(int(time.time())))
